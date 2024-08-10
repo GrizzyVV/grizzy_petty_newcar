370710040e33ddc7035708d18edb51c6d68885a8
GrizzyVV <84356682+GrizzyVV@users.noreply.github.com>
2024-08-10 10:51:30 -0400

    Update client.lua

--- grizzy_petty_newcar/client.lua
+++ grizzy_petty_newcar/client.lua
@@ -4,18 +4,31 @@ local CarRngList = {
     [3] = 'asbo',
     --[4] = '', -- copy/paste and add more cars, also you can change the value below to one specific model if you like
 }
-RegisterNetEvent('CreateCarData', function(xPlayer)
-    local selectedModel = CarRngList[math.random(1, #CarRngList)]
-    local coords = GetEntityCoords(PlayerPedId())
-    ESX.Streaming.RequestModel(selectedModel, function() -- preps the model
-        ESX.Game.SpawnVehicle(selectedModel, vec3(coords.x, coords.y+3.0, coords.z+3.0), 0.0, function(vehHandle) -- change selectedModel here to 'asea' or w/e car you want if you don't want random selection of starters
-            local props = ESX.Game.GetVehicleProperties(vehHandle) -- gets the plate, mods, engine health, etc
-            if DoesEntityExist(vehHandle) then 
-                local auth = 'true'
-                Wait(500)
-                ESX.Game.DeleteVehicle(vehHandle)
-                TriggerServerEvent('InsertCarToDb', xPlayer, props, auth)
-            end
-        end)
-    end)
+
+
+local playerSpawned = false
+AddEventHandler('playerSpawned', function()
+    playerSpawned = true
 end)
+
+
+RegisterNetEvent('esx:playerLoaded', function(xPlayer, isNew, skin)
+    while xPlayer == nil do Wait(10) end
+    while playerSpawned == nil or playerSpawned == false do Wait(10) end
+    if isNew == true and playerSpawned == true and xPlayer ~= nil then 
+        local selectedModel = CarRngList[math.random(1, #CarRngList)]
+        local coords = GetEntityCoords(PlayerPedId())
+        ESX.Streaming.RequestModel(selectedModel, function() -- preps the model
+            ESX.Game.SpawnVehicle(selectedModel, vec3(coords.x, coords.y+3.0, coords.z+3.0), 0.0, function(vehHandle) -- change selectedModel here to 'asea' or w/e car you want if you don't want random selection of starters
+                local props = ESX.Game.GetVehicleProperties(vehHandle) -- gets the plate, mods, engine health, etc
+                if DoesEntityExist(vehHandle) then 
+                    local auth = 'true'
+                    Wait(500)
+                    ESX.Game.DeleteVehicle(vehHandle)
+                    TriggerServerEvent('InsertCarToDb', props, auth)
+                end
+            end)
+        end)
+    end
+end)
+
